a83aab31a43c197b71ca1d7fa62c48536870a0e5
Sajjad Murtaza <sajjadmurtaza.nxb@gmail.com>
2025-11-11 21:20:24 +0900

    feat: Add Acme module entry point
    - Require all ACME components in main module file
    - Provides single entry point for basket system

---
+++ lib/acme.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'acme/errors'
+require_relative 'acme/money'
+require_relative 'acme/product'
+require_relative 'acme/catalogue'
+require_relative 'acme/delivery_rules'
+require_relative 'acme/offers/base'
+require_relative 'acme/offers/bogo_half'
+require_relative 'acme/basket'
+
+# Acme Widget Co - Shopping Basket System
+module Acme
+end
